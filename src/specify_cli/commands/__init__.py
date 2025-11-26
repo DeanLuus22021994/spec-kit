3a8e3a9cb5e268182eee1b09e02852330199e84a
DeanLuus22021994 <dean.luus2021994@gmail.com>
2025-11-26 21:40:59 +0200

    feat: add initial CLI command implementations for check, init, and version

---
+++ src/specify_cli/commands/__init__.py
@@ -0,0 +1,7 @@
+"""Specify CLI commands."""
+
+from specify_cli.commands.check import check
+from specify_cli.commands.init import init
+from specify_cli.commands.version import version
+
+__all__ = ["check", "init", "version"]
